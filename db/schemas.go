3f0e5533678da2e315166e33669ea57d9f72e86d
Ahdeyyy <85193076+Ahdeyyy@users.noreply.github.com>
2024-03-29 17:47:53 +0100

    refactor: added compare and edit to both the profile and password item

--- db/schemas.go
+++ db/schemas.go
@@ -1,7 +1,5 @@
 package db
 
-import "golang.org/x/crypto/bcrypt"
-
 type Profile struct {
 	Id             int64
 	Name           string
@@ -14,18 +12,4 @@ type PasswordItem struct {
 	App      string // can also be a url
 	Password string
 	Note     string
-}
-
-// Compares two profiles p1 and p2 and edits p1 with the values of p2 if p1 and p2 are not equal
-// p1 calls the function and is mutated
-func (p *Profile) CompareAndEdit(profile Profile) {
-	if p.Name != profile.Name {
-		p.Name = profile.Name
-	}
-
-	if bcrypt.CompareHashAndPassword([]byte(p.MasterPassword), []byte(profile.MasterPassword)) != nil {
-		hashedPassword, _ := bcrypt.GenerateFromPassword([]byte(profile.MasterPassword), bcrypt.MinCost)
-		p.MasterPassword = string(hashedPassword)
-
-	}
 }
--- db/utils.go
+++ db/utils.go
@@ -3,6 +3,8 @@ package db
 import (
 	"crypto/rand"
 	"math/big"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 const (
@@ -23,3 +25,39 @@ func GenerateStrongPassword() string {
 	return string(b)
 }
 
+// Compares two profiles p1 and p2 and edits p1 with the values of p2 if p1 and p2 are not equal
+// p1 calls the function and is mutated
+func (p *Profile) CompareAndEdit(profile Profile) {
+	if p.Name != profile.Name {
+		p.Name = profile.Name
+	}
+
+	if bcrypt.CompareHashAndPassword([]byte(p.MasterPassword), []byte(profile.MasterPassword)) != nil {
+		hashedPassword, _ := bcrypt.GenerateFromPassword([]byte(profile.MasterPassword), bcrypt.MinCost)
+		p.MasterPassword = string(hashedPassword)
+
+	}
+}
+
+// compares two passwordItems p1 and p2 and edits p1 with the values of p2 if p1 and p2 are not equal
+// p1 calls the function and is mutated
+
+func (p *PasswordItem) CompareAndEdit(password PasswordItem) {
+	if p.Note != password.Note {
+		p.Note = password.Note
+	}
+
+	if bcrypt.CompareHashAndPassword([]byte(p.Password), []byte(password.Password)) != nil {
+		hashedPassword, _ := bcrypt.GenerateFromPassword([]byte(password.Password), bcrypt.MinCost)
+		p.Password = string(hashedPassword)
+	}
+
+	if p.User != password.User {
+		p.User = password.User
+	}
+
+	if p.App != password.App {
+		p.App = password.App
+	}
+
+}
